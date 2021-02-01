d97cfc3c90f44ea9f96b98234f91221989bb5d33
Ryuzoutamura <ryuzouqqq@hotmail.co.jp>
2021-02-01 10:58:21 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '4f52cdc8c8e6920525c76233cb9c448cd07e1c099117aa17e44dfbc78ddb8a69062fb6d7940711921787b02ac24849dd0f79a2a105b76f6893a0898eae8252ff'
